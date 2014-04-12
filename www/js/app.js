5066fbbacbc5cb56a906ca82a4c37bd8a3b4817f
Nicholas Otto <nwkotto@umich.edu>
2014-04-12 00:07:09 -0400

    Added Facebook login

--- www/js/app.js
+++ www/js/app.js
@@ -1,11 +1,11 @@
 // Ionic Starter App
 
 // angular.module is a global place for creating, registering and retrieving Angular modules
-// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// 'assassin' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-// 'starter.services' is found in services.js
-// 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.controllers', 'starter.services', 'firebase'])
+// 'assassin.services' is found in services.js
+// 'assassin.controllers' is found in controllers.js
+angular.module('assassin', ['ionic', 'assassin.controllers', 'assassin.services', 'firebase'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -24,14 +24,19 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services', '
   // Each state's controller can be found in controllers.js
   $stateProvider
 
+    // Each tab has its own nav history stack:
+    .state('login', {
+      url: '/login',
+      templateUrl: 'templates/login.html',
+      controller: 'LoginCtrl'
+    })
+
     // setup an abstract state for the tabs directive
     .state('tab', {
       url: "/tab",
       abstract: true,
       templateUrl: "templates/tabs.html"
     })
-
-    // Each tab has its own nav history stack:
 
     .state('tab.target', {
       url: '/target',
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,10 +1,35 @@
-angular.module('starter.controllers', [])
+angular.module('assassin.controllers', [])
 
-.controller('TargetCtrl', function($scope, $firebase) {
-	var assassin = new Firebase("https://vivid-fire-2947.firebaseio.com");
+.controller('AppCtrl', function($scope, $rootScope, $location, $firebaseSimpleLogin) {
+	// Firebase
+	$rootScope.assassin = new Firebase("https://vivid-fire-2947.firebaseio.com");
+	$rootScope.auth = $firebaseSimpleLogin($rootScope.assassin);
+
+	function _login() {
+		$location.path('/tab');
+	}
+
+	function _logout() {
+		$location.path('/tab/target');
+	}
+
+	function _error(err) {
+		console.log(err);
+	}
+
+	$rootScope.$on('$firebaseSimpleLogin:login', _login);
+	$rootScope.$on('$firebaseSimpleLogin:logout', _logout);
+	$rootScope.$on('$firebaseSimpleLogin:error', _error);
+})
+
+.controller('TargetCtrl', function($scope, $firebase, $rootScope) {
 	// ------ This binds the "wattup" scope item to an item in firebase --------
-	$scope.wattup = $firebase(assassin.child("wattup"));
+	$scope.wattup = $firebase($rootScope.assassin.child("wattup"));
 })
 
 .controller('ProfileCtrl', function($scope) {
+	$scope.whatever = "cool";
+})
+
+.controller('LoginCtrl', function($scope) {
 });
