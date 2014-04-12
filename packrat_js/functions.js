9bc39e09fca450a1aed7a48e35501938853c3017
Packrat386 <packrat386@gmail.com>
2014-04-11 23:15:40 -0400

    fuck yo indents

--- packrat_js/functions.js
+++ packrat_js/functions.js
@@ -8,42 +8,53 @@ var myDataRef = new Firebase('https://vivid-fire-2947.firebaseio.com/');
 var users = myDataRef.child('Users');
 
 function genUser(fbid){
-  var user = new Object();
-  user.fbid = fbid;
-  user.password = getWord();
-  user.pw_remaining = user.password;
-  user.pw_cracked = "";
-  user.key_cracked = false;
-  user.target = 'empty_target';
-  users.push(user);
-  console.log(user);
+    var user = new Object();
+    user.fbid = fbid;
+    user.password = getWord();
+    user.pw_remaining = user.password;
+    user.pw_cracked = "";
+    user.key_cracked = false;
+    user.target = 'empty_target';
+    users.push(user);
+    console.log(user);
 }
 
 function getPwChar(id){
-  var targetRef = users.child(id);
-  targetRef.once('value', function(snapshot){
-    var target = snapshot.val();
-    if (target.pw_remaining.length > 0) {
-      var i = getRandomInt(0, target.pw_remaining.length);
-      target.pw_cracked = target.pw_cracked + target.pw_remaining.charAt(i);
-      target.pw_remaining = target.pw_remaining.substring(0, i) + target.pw_remaining.substring(i+1);
-      for(key in target){
-        console.log(key + ": " + target[key] + "|");
-        console.log("   ");
-      }
-      targetRef.set(target);
-    }
-  });
+    var targetRef = users.child(id);
+    targetRef.once('value', function(snapshot){
+	var target = snapshot.val();
+	if (target.pw_remaining.length > 0) {
+	    var i = getRandomInt(0, target.pw_remaining.length);
+	    target.pw_cracked = target.pw_cracked + target.pw_remaining.charAt(i);
+	    target.pw_remaining = target.pw_remaining.substring(0, i) + target.pw_remaining.substring(i+1);
+	    for(key in target){
+		console.log(key + ": " + target[key] + "|");
+		console.log("   ");
+	    }
+	    targetRef.set(target);
+	}
+    });
 }
 
 function getCrackedChars(id){
-  var targetRef = users.child(id);
-  targetRef.on('value', function(snapshot){
-    var target = snapshot.val();
-    return target.pw_cracked;
-  });
+    var targetRef = users.child(id);
+    targetRef.once('value', function(snapshot){
+	var target = snapshot.val();
+	return target.pw_cracked;
+    });
 }
 
-function killUserById(id) {
-  // target.cracked = true
+function killUser(target_id, user_id) {
+    var targetRef = users.child(target_id);
+    targetRef.once('value', function(snapshot){
+	var target = snapshot.val();
+	target.key_cracked = true;
+	var holder = target.target;
+	target.target = "empty";
+	var userRef = users.child(user_id);
+	userRef.once('value', function(snapshot){
+	    var user = snapshot;
+	    user.target =  holder;
+	});
+    });
 }
