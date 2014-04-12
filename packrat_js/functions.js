3ad862c3145f2c3c08de881f0c06bf9b41fa8d28
Jason Berlinsky <jason@barefootcoders.com>
2014-04-11 23:04:35 -0400

    Fix yo indentation

--- packrat_js/functions.js
+++ packrat_js/functions.js
@@ -8,36 +8,36 @@ var myDataRef = new Firebase('https://vivid-fire-2947.firebaseio.com/');
 var users = myDataRef.child('Users');
 
 function genUser(name, picture){
-    var user = new Object();
-    user.name = name;
-    user.picture = picture;
-    user.password = getWord();
-    user.pw_remaining = user.password;
-    user.pw_cracked = "";
-    user.key_cracked = false;
-    users.push(user);
-    document.write(user);
+  var user = new Object();
+  user.name = name;
+  user.picture = picture;
+  user.password = getWord();
+  user.pw_remaining = user.password;
+  user.pw_cracked = "";
+  user.key_cracked = false;
+  users.push(user);
+  document.write(user);
 }
 
 function getPwChar(id){
-    var targetRef = users.child(id);
-    targetRef.once('value', function(snapshot){
-	var target = snapshot.val();
-	var i = getRandomInt(0, target.pw_remaining.length);
-	target.pw_cracked = target.pw_cracked + target.pw_remaining.charAt(i);
-	target.pw_remaining = target.pw_remaining.substring(0, i) + target.pw_remaining.substring(i+1);
-	for(key in target){
-	    document.write(key + ": " + target[key] + "|");
-	    document.write("   ");
-	}
-	targetRef.set(target);
-    });
+  var targetRef = users.child(id);
+  targetRef.once('value', function(snapshot){
+    var target = snapshot.val();
+    var i = getRandomInt(0, target.pw_remaining.length);
+    target.pw_cracked = target.pw_cracked + target.pw_remaining.charAt(i);
+    target.pw_remaining = target.pw_remaining.substring(0, i) + target.pw_remaining.substring(i+1);
+    for(key in target){
+      document.write(key + ": " + target[key] + "|");
+      document.write("   ");
+    }
+    targetRef.set(target);
+  });
 }
 
 function getCrackedChars(id){
-    var targetRef = users.child(id);
-    targetRef.on('value', function(snapshot){
-	var target = snapshot.val();
-	return target.pw_cracked;
-    });
+  var targetRef = users.child(id);
+  targetRef.on('value', function(snapshot){
+    var target = snapshot.val();
+    return target.pw_cracked;
+  });
 }
